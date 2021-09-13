3466c151abda9fde42cf6725731c2c8888613592
richa <richardnham2000@gmail.com>
2021-09-13 16:35:21 -0400

    Final Solution

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scannerUserInput= new Scanner(System.in);
+        double euros;
+        double rate;
+
+        System.out.println("How many euros are you exchanging?");
+        euros =  scannerUserInput.nextInt();
+
+        System.out.println("What is the exchange rate?");
+        rate =  scannerUserInput.nextInt();
+
+        double usd = euros * rate;
+
+        usd = Math.ceil(usd * 100) / 100.0;
+
+        System.out.println(euros + " at the exchange rate of "+rate+"is");
+        System.out.println(usd + " U.S. Dollars");
+
+        scannerUserInput.close();
+    }
+
+}

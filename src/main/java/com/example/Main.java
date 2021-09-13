e83bb7a4d6493fb9ca7f7717f054ad5f8ee2ff53
richa <richardnham2000@gmail.com>
2021-09-13 16:39:14 -0400

    Final Solution

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,10 @@ package com.example;
 
 import java.util.Scanner;
 import java.lang.Math;
-
+/*
+ *  UCF COP3330 Fall 2021 Assignment 11 Solution
+ *  Copyright 2021 Richard Nham
+ */
 public class Main {
 
     public static void main(String[] args) {
